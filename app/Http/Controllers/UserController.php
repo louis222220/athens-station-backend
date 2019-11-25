4ea80f7def3ba9fb248e06c97d6459873036550f
charleen-xu <qnhsu@mail2.nccu.tw>
2019-11-25 13:42:15 +0800

    bearer token done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,85 +11,89 @@ use Str;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
 
-        $username =$request->username;
+        $username = $request->username;
         $password = $request->password;
         $role = $request->role;
 
         $inputs = $request->all();
 
         $rules = [
-            'username'=>'required',
-            'password'=>'required',
-            'role'=>'required'
+            'username' => 'required',
+            'password' => 'required',
+            'role' => 'required'
         ];
 
-        $validator = Validator::make($inputs,$rules);
+        $validator = Validator::make($inputs, $rules);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
 
-            return response(['message'=>$validator->errors()],409);
-        }else{
+            return response(['message' => $validator->errors()], 409);
+        } else {
 
-            $addRole = Role::create(['name'=>$role]);
+            $addRole = Role::create(['name' => $role]);
 
-            $addUser = User::create(['username'=>$username,
-            'password'=>Hash::make($request->password),
-            'role'=>$role,
-            'role_id'=>$addRole['id'],
-            'api_token'=>Str::random(15)]);
-
-    }
-
-    $result = [
-        'role'=>$role,
-        'username' => $username,
-        'password'=>$password
-
-   ];
-   return response()->json($result,201);
-
-}
-
-public function login(Request $request){
-
-    $username =$request->username;
-    $password = $request->password;
-
-    $inputs = $request->all();
-
-    $rules = [
-        'username'=>'required',
-        'password'=>'required',
-    ];
-
-    $validator = Validator::make($inputs,$rules);
-
-    if($validator->fails()){
-
-        return response(['message'=>$validator->errors()]);
-    }else{
-
-        $findUser = User::where('username',$username)->first();
-
-        if(!$findUser){
-            return response(['message'=>'Username not found'],404);
+            $addUser = User::create([
+                'username' => $username,
+                'password' => Hash::make($request->password),
+                'role' => $role,
+                'role_id' => $addRole['id'],
+                'api_token' => Str::random(15)
+            ]);
         }
 
-        $passwordHash = $findUser->password;
+        $result = [
+            'role' => $role,
+            'username' => $username,
+            'password' => $password
 
-        if (Hash::check($password,  $passwordHash)) {
+        ];
+        return response()->json($result, 201);
+    }
 
-            return response(['message'=>'嗨，好久不見：'.$username.'，您的資料如下：',
-            'data'=>$findUser],200);
-        }else{
-            return response(['message'=>'Wrong password'],401);
+    public function login(Request $request)
+    {
 
+        $username = $request->username;
+        $password = $request->password;
+
+        $inputs = $request->all();
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required',
+        ];
+
+        $validator = Validator::make($inputs, $rules);
+
+        if ($validator->fails()) {
+
+            return response(['message' => $validator->errors()]);
+        } else {
+
+            $findUser = User::where('username', $username)->first();
+
+            if (!$findUser) {
+                return response(['message' => 'Username not found'], 404);
+            }
+
+            $passwordHash = $findUser->password;
+
+            if (Hash::check($password,  $passwordHash)) {
+
+                return response([
+                    'message' => '嗨，好久不見：' . $username . '，您的資料如下：',
+                    'data' => $findUser
+                ], 200);
+            } else {
+                return response(['message' => 'Wrong password'], 401);
+            }
         }
-
     }
 
+    public function index(){
+        return User::all();
     }
-
 }
