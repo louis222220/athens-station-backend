84f38f1a771262d36701b0cd1821be649fe6a940
Louis Su <louis222220@gmail.com>
2019-11-26 19:52:34 +0800

    Add Constant

---
+++ app/Http/Controllers/Constant/GoodStatusConst.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Constant;
+
+return [
+    'praparing' => '準備中',
+    'shipping' => '運送中',
+    'arrived' => '已抵達',
+    'halfArrived' => '抵達中繼站'
+];
---
+++ app/Http/Controllers/Constant/ShipmentStatusConst.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers\Constant;
+
+return [
+    'preparing' => '準備中',
+    'shipping' => '運送中',
+    'arrived' => '已抵達'
+];
