174e544f3b8f7dd0124e36c5bbe320a6aecc114b
charleen-xu <qnhsu@mail2.nccu.tw>
2019-11-26 21:03:35 +0800

    checkoutApi

--- routes/api.php
+++ routes/api.php
@@ -36,6 +36,8 @@ Route::group(['middleware' => ['auth:api']], function () {
 
     Route::post('/checkin','ShipmentController@checkin');
 
+    Route::post('/checkout','ShipmentController@checkout');
+
 
 });
 
