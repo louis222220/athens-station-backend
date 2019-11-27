4672b2435eaedf63633ebb2ef09a46fb31a6ad0c
Louis Su <louis222220@gmail.com>
2019-11-27 15:20:47 +0800

    Add weight & price attribuute of Shipment

--- app/Shipment.php
+++ app/Shipment.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Good;
 
 class Shipment extends Model
 {
@@ -10,11 +11,26 @@ class Shipment extends Model
         'good_id','runner_id','start_station_id','des_station_id','status','good_name'
     ];
 
+    protected $appends = ['weight', 'price'];
 
     public function good()
     {
         return $this->belongsTo('App\Good');
     }
+
+
+    public function getWeightAttribute()
+    {
+        $good = Good::find($this->attributes['good_id']);
+        return (int) $good->weight;
+    }
+
+
+    public function getPriceAttribute()
+    {
+        return 20;
+    }
+
 
     // protected $appends = ['goodName'];
 
