f4a31627f736dc7648cc30bc1fbf53875817fd51
charleen-xu <qnhsu@mail2.nccu.tw>
2019-11-27 11:26:18 +0800

    remove store of AchivementController

--- app/Http/Controllers/AchivementController.php
+++ app/Http/Controllers/AchivementController.php
@@ -37,43 +37,7 @@ class AchivementController extends Controller
      */
     public function store(Request $request)
     {
-        $login_id = Auth::user()->id;
 
-        $get_shipment_data = Shipment::where('runner_id', $login_id)->get();
-        foreach ($get_shipment_data as $data) {
-
-            $total = $data->start_station_id + $data->des_station_id;
-
-            switch ($total) {
-                case 3:
-                    Achivement::create([
-                        'runner_id' => $login_id,
-                        'distance' => 300
-                    ]);
-                    return response()->json([
-                        'message' => '跑者成就增加', 'data' => Achivement::all()
-                    ]);
-
-                case 5:
-                    Achivement::create([
-                        'runner_id' => $login_id,
-                        'distance' => 500
-                    ]);
-                    return response()->json([
-                        'message' => '跑者成就增加', 'data' => Achivement::all()
-                    ]);
-
-                case 7:
-                    Achivement::create([
-                        'runner_id' => $login_id,
-                        'distance' => 700
-                    ]);
-                    return response()->json([
-                        'message' => '跑者成就增加', 'data' => Achivement::all()
-                    ]);
-            }
-        }
-        // $get_shipment_des_id = $get_shipment_data->
 
 
 
