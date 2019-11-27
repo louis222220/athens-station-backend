5e7ea6c43c378703d55aaf001f2e5c4139036d82
Louis Su <louis222220@gmail.com>
2019-11-27 17:25:29 +0800

    Add station_name attribute to Shipment

--- app/Shipment.php
+++ app/Shipment.php
@@ -11,11 +11,25 @@ class Shipment extends Model
         'good_id','runner_id','start_station_id','des_station_id','status','good_name'
     ];
 
-    protected $appends = ['weight', 'price', 'photo_url'];
+    protected $appends = ['weight', 'price', 'photo_url',
+     'start_station_name', 'des_station_name',
+    ];
 
     public function good()
     {
         return $this->belongsTo('App\Good');
+    }
+
+
+    public function getStartStationNameAttribute()
+    {
+        return Station::find($this->start_station_id)->name;
+    }
+
+
+    public function getDesStationNameAttribute()
+    {
+        return Station::find($this->des_station_id)->name;
     }
 
     
